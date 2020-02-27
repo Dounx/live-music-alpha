45cf697c7dbd870f4b3ff376ca39dda9282997d6
Dounx <imdounx@gmail.com>
2020-02-27 09:28:57 +0800

    Add action cable allowed request domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # Need to be server domain
+  config.action_cable.allowed_request_origins = ['http://106.12.191.133']
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
